a82f854b93a7401f5822ce379d67d8e66f1edfc2
kazimanil <kazimanileren@gmail.com>
2026-09-02 14:51:53 +0200

    add 2017 and remove 2013-4

--- Ready_data_Ologit.R
+++ Ready_data_Ologit.R
@@ -12,6 +12,146 @@ library("plotly")
 # The Data is shared by TURKSTAT on the promise that it will not be shared publicly.
 # Thus, I will only be able share aggregated data after the manipulation & aggregation steps.
 
+## 2017
+data_2017 <- fread("data/data_2017.csv")
+ologit_2017 <- data_2017[, .(
+    weight = FAKTOR_FERT,
+    hap = scale_transformation(MUTLULUK),
+    hap_binary = happiness_transformation(MUTLULUK),
+    sex = relevel(
+        as.factor(gender(CINSIYET)),
+        "Female"
+    ),
+    age = as.numeric(BITIRILEN_YAS),
+    age2 = as.numeric(BITIRILEN_YAS)^2,
+    mars = relevel(
+        as.factor(
+            marriage_satisfaction(
+                marital_status(MEDENI_DURUM),
+                scale_transformation(OLCEK_MEMNUNIYET_EVLILIK)
+            )
+        ),
+        "Single"
+    ),
+    jobs = relevel(
+        as.factor(
+            job_satisfaction(
+                employment(CALISMA_DURUM, CALISMAMA_NEDEN, TRUE),
+                scale_transformation(OLCEK_MEMNUNIYET_IS)
+            )
+        ),
+        "Out of Labour Force"
+    ),
+    mat = relevel(
+        as.factor(materialism(NE_MUTLU)),
+        "Not Materialistic"
+    ),
+    sw = relevel(
+        as.factor(paste0("Subjective Welfare - Cantril Ladder: ", as.character(as.numeric(UMUT_BASAMAK) - 1))),
+        "Subjective Welfare - Cantril Ladder: 0"
+    ),
+    hhi_lvl = relevel(
+        as.factor(household_income_transformation(GELIR_GRUP)),
+        "Household Income Tier 1"
+    ),
+    hhi_suf = relevel(
+        as.factor(likert_categoric(OLCEK_GELIR_KARSILAMA, "Income Sufficiency")),
+        "Neutral"
+    ),
+    sh = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SAGLIK, "Subjective Health")),
+        "Neutral"
+    ),
+    eds = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_EGITIM, "Education")),
+        "Neutral"
+    ),
+    hos = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KONUT, "Housing")),
+        "Neutral"
+    ),
+    dis = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SEMT, "Neighbourhood")),
+        "Neutral"
+    ),
+    hhi_sat = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_GELIR, "Income")),
+        "Neutral"
+    ),
+    frs = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_ARKADAS, "Friends")),
+        "Neutral"
+    ),
+    nes = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KOMSU, "Neighbours")),
+        "Neutral"
+    ),
+    sls = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SOS_HAYAT, "Social Life")),
+        "Neutral"
+    ),
+    tul = relevel(
+        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KISISEL_BKM, "Leisure")),
+        "Neutral"
+    ),
+    psh = relevel(
+        as.factor(likert_categoric(GUVEN_EV, "Safety at Home")),
+        "Neutral"
+    ),
+    pse = relevel(
+        as.factor(likert_categoric(GUVEN_CEVRE, "Safety Around Home")),
+        "Neutral"
+    ),
+    doh = relevel(
+        as.factor(hope_transformation(UMUT)),
+        "Not Hopeful At All"
+    ),
+    fya = relevel(
+        as.factor(comparison_5y_ago(GECMIS_KARSILASTIRMA)),
+        "No Idea"
+    ),
+    fyl = relevel(
+        as.factor(expectations_5y_later(GELECEK_KARSILASTIRMA)),
+        "No Idea"
+    ),
+    lyp = relevel(
+        as.factor(
+            ifelse(pmin(ISE_GIRDI, ISYERI_ACTI, GELIRI_ARTTI, TASARRUF_YAPTI, ARABA_ALDI, EV_ALDI, BORC_ODEDI, na.rm = TRUE) == 1,
+                "Positive Memory",
+                "No Positive Memory"
+            )
+        ),
+        "No Positive Memory"
+    ),
+    lyn = relevel(
+        as.factor(
+            ifelse(pmin(ISINI_KAYBETTI, IFLAS_ETTI, GELIRI_AZALDI, TASARRUFLARI_AZALDI, UCUZ_URUN, TATIL_KISTI, ARABA_SATTI, EV_SATTI, BORCLANDI, na.rm = TRUE) == 1,
+                "Negative Memory",
+                "No Negative Memory"
+            )
+        ),
+        "No Negative Memory"
+    ),
+    lym = relevel(
+        as.factor(
+            ifelse(GOC_ETTI == 1,
+                "Migrated",
+                "Not Migrated"
+            )
+        ),
+        "Not Migrated"
+    ),
+    fsp = relevel(
+        as.factor(
+            ifelse(pmax(CINSIYET_BASKI, MEDENI_DURUM_BASKI, YAS_BASKI, GELENEK_GORENEK, DINI_INANCDAN, SIYASI_GORUSTEN, MEMLEKETINDEN, ISINDEN_DOLAYI, KILIK_KIYAFET, ISSIZ_OLMA, GELIR_DUZEY_DURUM, na.rm = TRUE) > 3,
+                "Had Social Pressure",
+                "Base"
+            )
+        ),
+        "Had Social Pressure"
+    )
+)]
+
 ## 2016
 data_2016 <- fread("data/data_2016.csv")
 ologit_2016 <- data_2016[, .(
@@ -300,295 +440,9 @@ ologit_2015 <- data_2015[, .(
     )
 )]
 
-## 2014
-data_2014 <- fread("data/data_2014.csv")
-ologit_2014 <- data_2014[, .(
-    weight = ff,
-    hap = scale_transformation(MUTLULUK),
-    hap_binary = happiness_transformation(MUTLULUK),
-    sex = relevel(
-        as.factor(gender(CINSIYET)),
-        "Female"
-    ),
-    age = as.numeric(BITIRILEN_YAS),
-    age2 = as.numeric(BITIRILEN_YAS)^2,
-    mars = relevel(
-        as.factor(
-            marriage_satisfaction(
-                marital_status(MEDENI_DURUM),
-                scale_transformation(OLCEK_MEMNUNIYET_EVLILIK)
-            )
-        ),
-        "Single"
-    ),
-    edl = relevel(
-        as.factor(education_level(OKUL_BITEN)),
-        "No Schooling"
-    ),
-    jobs = relevel(
-        as.factor(
-            job_satisfaction(
-                employment(CALISMA_DURUM, CALISMAMA_NEDEN, TRUE),
-                scale_transformation(OLCEK_MEMNUNIYET_IS)
-            )
-        ),
-        "Out of Labour Force"
-    ),
-    mat = relevel(
-        as.factor(materialism(NE_MUTLU)),
-        "Not Materialistic"
-    ),
-    sw = relevel(
-        as.factor(paste0("Subjective Welfare - Cantril Ladder: ", as.character(as.numeric(UMUT_BASAMAK) - 1))),
-        "Subjective Welfare - Cantril Ladder: 0"
-    ),
-    hhi_lvl = relevel(
-        as.factor(household_income_transformation(GELIR_GRUP)),
-        "Household Income Tier 1"
-    ),
-    hhi_suf = relevel(
-        as.factor(likert_categoric(OLCEK_GELIR_KARSILAMA, "Income Sufficiency")),
-        "Neutral"
-    ),
-    sh = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SAGLIK, "Subjective Health")),
-        "Neutral"
-    ),
-    eds = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_EGITIM, "Education")),
-        "Neutral"
-    ),
-    hos = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KONUT, "Housing")),
-        "Neutral"
-    ),
-    dis = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SEMT, "Neighbourhood")),
-        "Neutral"
-    ),
-    hhi_sat = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_GELIR, "Income")),
-        "Neutral"
-    ),
-    frs = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_ARKADAS, "Friends")),
-        "Neutral"
-    ),
-    nes = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KOMSU, "Neighbours")),
-        "Neutral"
-    ),
-    sls = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SOS_HAYAT, "Social Life")),
-        "Neutral"
-    ),
-    tul = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KISISEL_BKM, "Leisure")),
-        "Neutral"
-    ),
-    psh = relevel(
-        as.factor(likert_categoric(GUVEN_EV, "Safety at Home")),
-        "Neutral"
-    ),
-    pse = relevel(
-        as.factor(likert_categoric(GUVEN_CEVRE, "Safety Around Home")),
-        "Neutral"
-    ),
-    doh = relevel(
-        as.factor(hope_transformation(UMUT)),
-        "Not Hopeful At All"
-    ),
-    fya = relevel(
-        as.factor(comparison_5y_ago(GECMIS_KARSILASTIRMA)),
-        "No Idea"
-    ),
-    fyl = relevel(
-        as.factor(expectations_5y_later(GELECEK_KARSILASTIRMA)),
-        "No Idea"
-    ),
-    lyp = relevel(
-        as.factor(
-            ifelse(pmin(ISE_GIRDI, ISYERI_ACTI, GELIRI_ARTTI, TASARRUF_YAPTI, ARABA_ALDI, EV_ALDI, BORC_ODEDI, na.rm = TRUE) == 1,
-                "Positive Memory",
-                "No Positive Memory"
-            )
-        ),
-        "No Positive Memory"
-    ),
-    lyn = relevel(
-        as.factor(
-            ifelse(pmin(ISINI_KAYBETTI, IFLAS_ETTI, GELIRI_AZALDI, TASARRUFLARI_AZALDI, UCUZ_URUN, TATIL_KISTI, ARABA_SATTI, EV_SATTI, BORCLANDI, na.rm = TRUE) == 1,
-                "Negative Memory",
-                "No Negative Memory"
-            )
-        ),
-        "No Negative Memory"
-    ),
-    lym = relevel(
-        as.factor(
-            ifelse(GOC_ETTI == 1,
-                "Migrated",
-                "Not Migrated"
-            )
-        ),
-        "Not Migrated"
-    ),
-    fsp = relevel(
-        as.factor(
-            ifelse(pmax(CINSIYET_BASKI, MEDENI_DURUM_BASKI, YAS_BASKI, GELENEK_GORENEK, DINI_INANCDAN, SIYASI_GORUSTEN, MEMLEKETINDEN, ISINDEN_DOLAYI, KILIK_KIYAFET, ISSIZ_OLMA, GELIR_DUZEY_DURUM, na.rm = TRUE) > 3,
-                "Had Social Pressure",
-                "Base"
-            )
-        ),
-        "Had Social Pressure"
-    )
-)]
 
-
-## 2013
-data_2013 <- fread("data/data_2013.csv")
-
-ologit_2013 <- data_2013[, .(
-    weight = FAKTOR_FERT,
-    hap = scale_transformation(MUTLULUK),
-    hap_binary = happiness_transformation(MUTLULUK),
-    sex = relevel(
-        as.factor(gender(CINSIYET)),
-        "Female"
-    ),
-    age = as.numeric(BITIRILEN_YAS),
-    age2 = as.numeric(BITIRILEN_YAS)^2,
-    mars = relevel(
-        as.factor(
-            marriage_satisfaction(
-                marital_status(MEDENI_DURUM),
-                scale_transformation(OLCEK_MEMNUNIYET_EVLILIK)
-            )
-        ),
-        "Single"
-    ),
-    edl = relevel(
-        as.factor(education_level(OKUL_BITEN)),
-        "No Schooling"
-    ),
-    jobs = relevel(
-        as.factor(
-            job_satisfaction(
-                employment(CALISMA_DURUM, CALISMAMA_NEDEN, TRUE),
-                scale_transformation(OLCEK_MEMNUNIYET_IS)
-            )
-        ),
-        "Out of Labour Force"
-    ),
-    mat = relevel(
-        as.factor(materialism(NE_MUTLU)),
-        "Not Materialistic"
-    ),
-    sw = relevel(
-        as.factor(paste0("Subjective Welfare - Cantril Ladder: ", as.character(as.numeric(UMUT_BASAMAK) - 1))),
-        "Subjective Welfare - Cantril Ladder: 0"
-    ),
-    hhi_lvl = relevel(
-        as.factor(household_income_transformation(GELIR_GRUP)),
-        "Household Income Tier 1"
-    ),
-    hhi_suf = relevel(
-        as.factor(likert_categoric(OLCEK_GELIR_KARSILAMA, "Income Sufficiency")),
-        "Neutral"
-    ),
-    sh = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SAGLIK, "Subjective Health")),
-        "Neutral"
-    ),
-    eds = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_EGITIM, "Education")),
-        "Neutral"
-    ),
-    hos = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KONUT, "Housing")),
-        "Neutral"
-    ),
-    dis = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SEMT, "Neighbourhood")),
-        "Neutral"
-    ),
-    hhi_sat = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_GELIR, "Income")),
-        "Neutral"
-    ),
-    frs = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_ARKADAS, "Friends")),
-        "Neutral"
-    ),
-    nes = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KOMSU, "Neighbours")),
-        "Neutral"
-    ),
-    sls = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_SOS_HAYAT, "Social Life")),
-        "Neutral"
-    ),
-    tul = relevel(
-        as.factor(likert_categoric(OLCEK_MEMNUNIYET_KISISEL_BKM, "Leisure")),
-        "Neutral"
-    ),
-    psh = relevel(
-        as.factor(likert_categoric(GUVEN_EV, "Safety at Home")),
-        "Neutral"
-    ),
-    pse = relevel(
-        as.factor(likert_categoric(GUVEN_CEVRE, "Safety Around Home")),
-        "Neutral"
-    ),
-    doh = relevel(
-        as.factor(hope_transformation(UMUT)),
-        "Not Hopeful At All"
-    ),
-    fya = relevel(
-        as.factor(comparison_5y_ago(GECMIS_KARSILASTIRMA)),
-        "No Idea"
-    ),
-    fyl = relevel(
-        as.factor(expectations_5y_later(GELECEK_KARSILASTIRMA)),
-        "No Idea"
-    ),
-    lyp = relevel(
-        as.factor(
-            ifelse(pmin(ISE_GIRDI, ISYERI_ACTI, GELIRI_ARTTI, TASARRUF_YAPTI, ARABA_ALDI, EV_ALDI, BORC_ODEDI, na.rm = TRUE) == 1,
-                "Positive Memory",
-                "No Positive Memory"
-            )
-        ),
-        "No Positive Memory"
-    ),
-    lyn = relevel(
-        as.factor(
-            ifelse(pmin(ISINI_KAYBETTI, IFLAS_ETTI, GELIRI_AZALDI, TASARRUFLARI_AZALDI, UCUZ_URUN, TATIL_KISTI, ARABA_SATTI, EV_SATTI, BORCLANDI, na.rm = TRUE) == 1,
-                "Negative Memory",
-                "No Negative Memory"
-            )
-        ),
-        "No Negative Memory"
-    ),
-    lym = relevel(
-        as.factor(
-            ifelse(GOC_ETTI == 1,
-                "Migrated",
-                "Not Migrated"
-            )
-        ),
-        "Not Migrated"
-    ),
-    fsp = relevel(
-        as.factor(
-            ifelse(pmax(CINSIYET_BASKI, MEDENI_DURUM_BASKI, YAS_BASKI, GELENEK_GORENEK, DINI_INANCDAN, SIYASI_GORUSTEN, MEMLEKETINDEN, ISINDEN_DOLAYI, KILIK_KIYAFET, ISSIZ_OLMA, GELIR_DUZEY_DURUM, na.rm = TRUE) > 3,
-                "Had Social Pressure",
-                "Base"
-            )
-        ),
-        "Had Social Pressure"
-    )
-)]
-
+# Logistic Regressions ----
 # Only works for 0/1 y variables. Not Ologit.
-summary(glm(hap_binary ~ sex + age + age2 + mars + jobs + mat + lyp + lyn + lym + fsp, data = ologit_2016, family = "binomial"))
+ologit_2017_results <- summary(glm(hap_binary ~ sex + age + age2 + mars + jobs + mat + sh + hos + dis + lyp + lyn + lym + fsp, data = ologit_2017, family = "binomial"))
+ologit_2016_results <- summary(glm(hap_binary ~ sex + age + age2 + mars + jobs + mat + sh + hos + dis + lyp + lyn + lym + fsp, data = ologit_2016, family = "binomial"))
+ologit_2015_results <- summary(glm(hap_binary ~ sex + age + age2 + mars + jobs + mat + sh + hos + dis + lyp + lyn + lym + fsp, data = ologit_2015, family = "binomial"))
